ccc7de522521b5741e291c26eeee744c031b6072
Dima A <dimaa@dimaa.ru>
2020-10-01 08:39:48 +0300

    add open menu

--- js/index.js
+++ js/index.js
@@ -2,37 +2,50 @@ const btnBurgerr  = document.querySelector('.btn-burger');
 const catalog = document.querySelector('.catalog');
 const overlay = document.querySelector('.overlay');
 const btnClose = document.querySelector('.btn-close');
-const catalogList = document.querySelector('.catalog-list');
-const subCatalog = document.querySelector('.subcatalog')
+//const catalogList = document.querySelector('.catalog-list');
+const subCatalog = document.querySelector('.subcatalog');
+const subcatalogHeader =document.querySelector('.subcatalog-header');
 
 const openMenu = () => {
     catalog.classList.add('open');
     overlay.classList.add('active');
     console.log('open');
 }
+
 const closeMenu = () => {
     catalog.classList.remove('open');
     overlay.classList.remove('active');
+    subCatalog.classList.remove('subopen')
+
     console.log('close');
 }
 
 const openSubMenu = event => {
     event.preventDefault();
+
+    const itemTarget = event.target.closest('.catalog-list__item');
+    if(itemTarget){
+        subcatalogHeader.innerHTML= itemTarget.innerHTML;
+        subCatalog.classList.add('subopen')
+    }
 }
 
+// open main menu
 btnBurgerr.addEventListener('click', openMenu);
 //
 
+//
+// close main manu
+// overlay clicked
 overlay.addEventListener('click',closeMenu);
-//
+// button Close clicked
 btnClose.addEventListener('click', closeMenu);
-//
+// button Esc pressed
 document.addEventListener('keydown',event =>{
-    //event.preventDefault();
-    console.log(event.code)
+  
     if(event.code =='Escape'){
-        closeMenu()
+        closeMenu();
     }
 })
 //
-catalogList.addEventListener('click', openSubMenu);
+catalog.addEventListener('click', openSubMenu);
